ef7bef01a80a49b7d0de90989e78d8370d570980
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2024-01-21 23:47:39 -0600

    Update zz_sql_practice_27.sql

--- zz_sql_practice_27.sql
+++ zz_sql_practice_27.sql
@@ -13,3 +13,21 @@ where t.student_id not in (
     select student_id from temp where
     score=max_score or score=min_score
 )
+
+
+---
+with temp as(
+    select customer_id,
+    sum(case when product_name='A' then 1 else 0 end) over(partition by customer_id) as A_purchasecount,
+    sum(case when product_name='B' then 1 else 0 end) over(partition by customer_id) as B_purchasecount,
+    sum(case when product_name='C' then 1 else 0 end) over(partition by customer_id) as C_purchasecount
+    from Orders
+)
+
+select distinct c.customer_id, c.customer_name from 
+Customers c left join temp on
+c.customer_id=temp.customer_id
+where temp.A_purchasecount > 0 and
+temp.B_purchasecount > 0 and 
+temp.C_purchasecount = 0
+order by customer_id;
