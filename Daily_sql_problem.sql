645d0c3daefb43e4158d9f53059357e43ec8b9c9
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2024-02-02 23:35:08 -0600

    Update Daily_sql_problem.sql

--- Daily_sql_problem.sql
+++ Daily_sql_problem.sql
@@ -31,3 +31,37 @@ select id,company,salary from temp
 where rank_no = (count_no/2) + 1
 or (count_no%2 = 0 and rank_no = count_no/2)
 
+---02/01/2024----
+select d.dept_name,
+isnull(count(student_id),0) as student_number from
+Department d left join Student s on
+s.dept_id = d.dept_id
+group by d.dept_name
+order by 2 desc, d.dept_name asc;
+
+---02/02---
+select round(
+    case
+    when (select count(*) from (select distinct requester_id,accepter_id from RequestAccepted)r) !=0
+       and  (select count(*) from (select distinct sender_id,send_to_id from FriendRequest)s)!=0
+       then (select count(*) from (select distinct requester_id,accepter_id from RequestAccepted)r)*1.0 /
+       (select count(*) from (select distinct sender_id,send_to_id from FriendRequest)s)
+    else 0.00
+    end,
+    2
+) as accept_rate
+
+
+-- with temp as(
+--     select distinct s.sender_id,s.send_to_id,
+--     r.requester_id,r.accepter_id from
+--     FriendRequest s left join RequestAccepted r on
+--     s.sender_id=r.requester_id and
+--     s.send_to_id = r.accepter_id
+-- )
+
+-- select
+-- case
+-- when count(accepter_id)!=0 and count(sender_id)!=0 then round(count(accepter_id)*1.0/count(sender_id),2)
+-- else 0.00
+-- end as accept_rate from temp
