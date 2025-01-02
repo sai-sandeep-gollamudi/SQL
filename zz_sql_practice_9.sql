8092f466cecb70654a15fdecd11a5a1652951517
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2025-01-01 23:59:29 -0600

    Update zz_sql_practice_9.sql

--- zz_sql_practice_9.sql
+++ zz_sql_practice_9.sql
@@ -6,3 +6,13 @@ conditions like 'DIAB1%';
 delete p2 from Person p1,
 Person p2
 where p2.id>p1.id and p1.email=p2.email;
+
+
+# Write your MySQL query statement below
+select distinct viewer_id as id from(
+select 
+viewer_id,
+count(*) as ct
+from (select distinct * from views where author_id != viewer_id)p
+group by viewer_id, view_date
+)t where t.ct>1 order by t.viewer_id
